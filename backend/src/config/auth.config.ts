dc732bdc290e60403b19276b7202c8d1b276b23b
Shrey <sshrey0007@gmail.com>
2025-10-22 19:30:33 +0530

    fix: minor bug

--- backend/src/config/auth.config.ts
+++ backend/src/config/auth.config.ts
@@ -5,13 +5,13 @@ const authConfig = {
     refresh_secret: process.env.AUTH_REFRESH_SECRET as string, 
     refresh_secret_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
 
-    base_domain: process.env.BASE_DOMAIN as string
+    base_domain: process.env.NODE_ENV === "production" ? process.env.BASE_DOMAIN as string : "localhost"
 }
 
 export const verifyConfig = () => {
     const keys = Object.keys(authConfig);
     for (let key of keys) {
-        if (!(key in authConfig)) {
+        if (!authConfig[key as keyof typeof authConfig]) {
             throw Error(`${key} absent from env`);
         }
     }
